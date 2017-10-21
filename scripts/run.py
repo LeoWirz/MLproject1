1a987704e8ea83096ae7858925fc63edc636ee76
LeoWirz <leonardo.wirz@epfl.ch>
2017-10-21 23:10:10 +0200

    run updated

--- scripts/run.py
+++ scripts/run.py
@@ -2,12 +2,26 @@ import numpy as np
 
 from proj1_helpers import *
 from implementations import *
+from validation import *
+from model_enum import *
+from feature_preprocessing import *
 
 TRAIN_FILE = '../data/train.csv'
 TEST_FILE = '../data/test.csv'
+CHOSEN_MODEL = Model.LEAST_SQUARES
+ONLY_SAMPLE = True
 
 if __name__ == "__main__":
+    # load training and testing sets
+    trainY,trainX,trainIds = load_csv_data(TRAIN_FILE, ONLY_SAMPLE)
+    testY,testX,testIds = load_csv_data(TEST_FILE, ONLY_SAMPLE)
 
-	# load training and testing sets
-    trainY,trainX,trainIds = load_csv_data(TRAIN_FILE)
-    testY,testX,testIds = load_csv_data(TEST_FILE)
+    #loss,score = k_fold_cross_validation(trainY, trainX, 2, CHOSEN_MODEL)
+
+    train_processed_x = featuring(trainX)
+	test_procedded_x = featuring(testX)
+
+	weight, loss = least_squares(trainY, train_processed_x)
+	y_pred = predict_labels(weight, test_procedded_x)
+
+	create_csv_submission(testIds,y_pred, "sumbmission.csv")
