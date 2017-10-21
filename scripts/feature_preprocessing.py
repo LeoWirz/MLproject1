41ad4cfac3a64de62517667e937aaba59885b0b3
LeoWirz <leonardo.wirz@epfl.ch>
2017-10-21 23:04:19 +0200

    feature expansion

---
+++ scripts/feature_preprocessing.py
@@ -0,0 +1,34 @@
+from proj1_helpers import *
+from implementations import *
+import numpy as np
+
+def featuring(features):
+    # replace -999 by nan
+    temp_feat = np.copy(features)
+    for x in temp_feat:
+        np.place(x, x == -999, np.nan)
+        
+    stacked_feat = np.hstack([temp_feat] 
+                             + [np.power(temp_feat,2)] 
+                             + [np.absolute(temp_feat)**0.5]
+                             + [np.arctan(temp_feat)]
+                             + [add_pairwise_products(temp_feat)])
+    inplace_map(np.nan_to_num, stacked_feat)
+    return stacked_feat
+
+    # add rows with squares et roots
+    stacked_feat = np.hstack([temp_feat] + [np.power(temp_feat,2)] + [np.absolute(temp_feat)**0.5])
+    inplace_map(np.nan_to_num, stacked_feat)
+    return stacked_feat
+    
+
+def inplace_map(f, x):
+    for i, v in enumerate(x):
+        x[i] = f(v)
+        
+def add_pairwise_products(x):
+    return np.hstack(
+        (x,
+        np.column_stack([x[:,i] * x[:,j] 
+        for i in range(0,30) 
+        for j in range(0,30) if i < j])))
